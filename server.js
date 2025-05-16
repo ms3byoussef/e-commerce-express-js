858d59fd866a668fb97fbbdce5fbcac382faf6bd
Khaled Abdelfatah Youssef <ms3byoussef@gmail.com>
2025-05-16 14:10:47 +0300

    Update dependencies and refactor MongoDB connection to use Mongoose. Added ecommerce-api and mongoose to package.json and package-lock.json. Replaced MongoClient with Mongoose for database connection in server.js.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const  mongoose = require("mongoose");
 
 dotenv.config("config.env")
 
@@ -13,30 +13,18 @@ if(process.env.NODE_ENV === "development"){
 }
 
 
-const uri = "mongodb+srv://ms3byoussef:<db_password>@e-commerce-node-js.b3g99rn.mongodb.net/?retryWrites=true&w=majority&appName=E-Commerce-node-js";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+mongoose.connect(
+    process.env.DB_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err.message);
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
